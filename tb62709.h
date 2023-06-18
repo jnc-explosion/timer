6b76355003e3a144a87978e94fdadda3b20a559b
jnc-explosion <himajin8634@gmail.com>
2023-06-18 10:04:36 +0900

    add: mplab base struct

---
+++ tb62709.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   tb62709.h
+ * Author: meumeumeucar
+ *
+ * Created on June 17, 2023, 6:57 PM
+ */
+
+#ifndef TB62709_H
+#define	TB62709_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* TB62709_H */
+
